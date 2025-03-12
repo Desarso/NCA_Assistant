8e2b813f47bdfce10dbe5808d3249f550bd804e0
desarso <malek.gabriel33@gmail.com>
2025-03-12 01:13:21 -0700

    getting rid of reload

--- backend/index.py
+++ backend/index.py
@@ -752,4 +752,4 @@ async def get_current_user_info(current_user: FirebaseUser = Depends(get_current
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("index:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("index:app", host="0.0.0.0", port=8000)
